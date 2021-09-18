e31960dc5969c5231ab43372eea1e626fd911e88
Lenny Thach <lennythach1@gmail.com>
2021-09-17 17:52:04 -0700

    Adding folder directories

--- MyBlogSite.py
+++ MyBlogSite.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Lenny Thach',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date_posted': 'September 17, 2021'
+    },
+
+    {
+        'author':'Meiling Sproger',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date_posted': 'September 18, 2021'
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__=='__main__':
